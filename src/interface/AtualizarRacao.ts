ec1e97da3fd0b35d5b1449ab7951d25918390e28
weslleycon10 <weslleycon10@gmail.com>
2024-09-16 10:59:27 +0000

    atualização na interface

---
+++ src/interface/AtualizarRacao.ts
@@ -0,0 +1,8 @@
+export default interface AtualizarRacao{
+    [x: string]: string | number | Date;
+        id: number,
+        nome: string,
+        kgQuantidade: number,
+        valorPago: number,
+        dataCompra: string
+    }
